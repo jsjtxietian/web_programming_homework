8550689cc6c62f016e61b98f453c3d4c7058142d
天 谢 <jsjtxietian@outlook.com>
2018-05-26 11:02:54 +0800

    update function

--- Code/BasicToDoMVC/main.js
+++ Code/BasicToDoMVC/main.js
@@ -1,70 +1,71 @@
 function $(id) {
     return document.getElementById(id);
 }
-
-var activeAmount = 0;
 const CL_COMPLETED = 'completed';
 
-function updateActive() {
-    var active = $('active');
-    active.innerHTML = activeAmount + ' items left';
+var items = [];
+
+function update() {
+    var list = $('list');
+    list.innerHTML = '';
+    for (var i = 0; i < items.length; ++i) {
+        (function (index) {
+            var it = items[index];
+            var msg = it.msg;
+            var item = document.createElement('div');
+            var itemContent = document.createElement('div');
+            var itemDelete = document.createElement('button');
+            item.classList.add('list-item');
+            it.completed && item.classList.add(CL_COMPLETED);
+            itemContent.innerHTML = msg;
+            itemContent.className = 'content';
+            itemDelete.type = 'button';
+            itemDelete.innerHTML = ' X ';
+            itemDelete.className = 'delete';
+            item.appendChild(itemContent);
+            item.appendChild(itemDelete);
+            list.insertBefore(item, list.childNodes[0]);
+
+            // bind events
+            item.addEventListener('click', function () {
+                it.completed = !it.completed;
+                update();
+            }, false);
+            itemDelete.addEventListener('click', function (event) {
+                items.splice(index, 1);
+                update();
+                event.stopPropagation();
+            }, false);
+        })(i);
+    }
+
+    var activeCount = 0;
+    items.forEach(function (item) {
+        if (!item.completed)
+            ++activeCount;
+    });
+    $('count').innerHTML = activeCount + ' items left';
 }
 
 function addTodo() {
     var todo = $('todo');
     var msg = todo.value;
-    var list = $('list');
-
     if (msg == '') {
         console.warn('msg is empty');
         return;
     }
-    var item = document.createElement('div');
-    var itemContent = document.createElement('div');
-    var itemDelete = document.createElement('button');
 
-    item.className = 'list-item';
-    itemContent.innerHTML = msg;
-    itemContent.className = 'content';
-    itemDelete.type = 'button';
-    itemDelete.innerHTML = 'X';
-    itemDelete.className = 'delete';
-
-    item.appendChild(itemContent);
-    item.appendChild(itemDelete);
-    list.insertBefore(item, list.childNodes[0]);
-
+    items.push({
+        msg: msg,
+        completed: false
+    });
+    update();
     todo.value = '';
-    ++activeAmount;
-    updateActive();
-
-    item.addEventListener('click', () => {
-        if (item.classList.contains(CL_COMPLETED)) {
-            item.classList.remove(CL_COMPLETED);
-            ++activeAmount;
-        }
-        else {
-            item.classList.add(CL_COMPLETED)
-            --activeAmount;
-        }
-        updateActive();
-    }, false);
-
-    itemDelete.addEventListener('click', function (event) {
-        list.removeChild(item);
-        if (!item.classList.contains(CL_COMPLETED))
-            --activeAmount;
-        updateActive();
-        event.stopPropagation();
-    }, false);
 }
 
-
 window.onload = () => {
-    // Binding keyboard event
     $('todo').addEventListener('keyup', function (event) {
-        //console.log('0x' + event.keyCode.toString(16));
-        if (event.keyCode != 0xd) return; // 0xd is "enter"
+        if (event.keyCode != 13) return;
         addTodo();
     }, false);
 }
