ab039c4a5372041cbd1cb7630c6b549262b2b990
天 谢 <jsjtxietian@outlook.com>
2018-06-09 14:07:24 +0800

    jsonp&ajax

--- Code/BasicToDoMVC/main.js
+++ Code/BasicToDoMVC/main.js
@@ -68,4 +68,115 @@ window.onload = () => {
         if (event.keyCode != 13) return;
         addTodo();
     }, false);
+
+    //jsonp
+    (function () {
+        var guid = 0;
+        /**
+         * Basic JSONP
+         * @param url {String}
+         * @param [opt] {Object}
+         * @param [opt.onSuccess] {Function}
+         * @param [opt.onFailure] {Function}
+         */
+        window.JSONP = function (url, opt) {
+            opt = opt || {};
+            var script = document.createElement('script');
+            var id = guid++;
+            var jsonp = '__jsonp_' + id;
+            window[jsonp] = function (res) {
+                if (opt.onSuccess) opt.onSuccess(res);
+                // define error & use opt.onFailure
+            };
+            script.onload = function () {
+                document.body.removeChild(script);
+                delete window[jsonp];
+            };
+
+            url += (url.indexOf('?') > -1 ? '&' : '?') + 'jsonp=' + jsonp;
+            script.setAttribute('src', url);
+            document.body.appendChild(script);
+        };
+    })();
+   
+    (function () {
+        var JSONP = window.JSONP;
+        var URL = 'http://127.0.0.1:3476';
+        var MSG = 'Start server by `node examples/data/server.js` on project root';
+
+        Object.assign(window, {
+            init: function (callback) {
+                JSONP(URL + '/init', {
+                    onSuccess: function (data) {
+                        console.log(data);
+                    },
+                    onFailure: function () {
+                        console.error(MSG);
+                    }
+                });
+            },
+            flush: function (callback) {
+                JSONP(URL + '/flush?data=' , {
+                    onSuccess: function (data) {
+                        console.log('flushed');
+                    },
+                    onFailure: function () {
+                        console.error(MSG);
+                    }
+                });
+            }
+        });
+    })();
 }
+
+//ajax demo
+(function () {
+    var GET = 'get';
+    window.Ajax = {
+        /**
+         * Basic Ajax
+         * @param url {String}
+         * @param [opt] {Object}
+         * @param [opt.onSuccess] {Function}
+         * @param [opt.onFailure] {Function}
+         * @param [opt.cached] {Boolean}
+         */
+        get: function (url, opt) {
+            opt = opt || {};
+            if (opt.cached) {
+                url += (url.indexOf('?') > -1 ? '&' : '?') + new Date().getTime() + 'r';
+            }
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (request.readyState === XMLHttpRequest.DONE) {
+                    if (request.status >= 200 && request.status < 400) {
+                        var ret = request.responseText;
+                        var contentType = request.getResponseHeader('content-type');
+                        if (contentType == 'application/json' || contentType == 'text/json') {
+                            try {
+                                ret = JSON.parse(ret);
+                            } catch (e) {
+                                console.error(e);
+                                if (opt.onFailure) opt.onFailure(e);
+                            }
+                        }
+                        if (opt.onSuccess) opt.onSuccess(ret);
+                    }
+                    else {
+                        if (opt.onFailure) opt.onFailure();
+                    }
+                }
+            };
+
+            request.open(GET, url, true);
+            request.send(null);
+        }
+    }
+})();
+
+// Ajax.get("https://httpbin.org/get", {
+//     onSuccess: (result) => {
+//         console.log(result);
+//     }
+// });
+
